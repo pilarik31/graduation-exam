9f213010f77487114257b78a895ff98858df4f28
John Pilar <pilarjan4111@gmail.com>
2021-05-07 19:30:41 +0200

    timers view

--- resources/views/tasks/partials/time.blade.php
+++ resources/views/tasks/partials/time.blade.php
@@ -2,6 +2,20 @@
     <div class="col-lg-12 col-md-12">
         <div class="shadow p-3 bg-white rounded timer">
             <timer task-id="{{ $task->id }}"></timer>
+
+
+            @foreach($task->timers as $timer)
+            <div class="display-time">
+                <strong>{{ $timer->name }}</strong>
+                <p>
+                    {{ date('d. m. Y | H:i:s', strtotime($timer->started_at)) }} 
+                    - 
+                    {{ date('d. m. Y | H:i:s', strtotime($timer->stopped_at)) }}
+                    &rarr;
+                    {{ $timer->stopped_at->diff($timer->started_at)->format('%h hodin %i minut %s sekund') }}
+                </p>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
